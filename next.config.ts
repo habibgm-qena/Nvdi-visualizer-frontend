44e5d81336622d9244753500049174c966d0e1a1
NerdRow <getnet.adugna@aait.edu.et>
2026-04-28 11:02:59 +0300

    chore: update Next.js version and adjust TypeScript configuration
    - Changed the Next.js version from 15.2.4 to 16.2.4 in package.json.
    - Removed the `--turbopack` flag from the dev script in package.json.
    - Updated TypeScript configuration in tsconfig.json:
      - Changed JSX setting from "preserve" to "react-jsx".
      - Added ".next/dev/types/**/*.ts" to the include paths.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
     outputFileTracingIncludes: {
         '/*': ['./registry/**/*']
     },
+    turbopack: {},
     images: {
         remotePatterns: [
             {
